002fa1a1b0bd303975a1a3fa192713d241c244d6
Artificialmaster918 <134881162+Artificialmaster918@users.noreply.github.com>
2023-10-07 15:27:13 +0300

    Add files via upload

---
+++ 03_Celsius_to_Fahrenheit.py
@@ -0,0 +1,8 @@
+# Formula: T (° F) = T (° C) × 1,8 + 32
+celsius = float(input())
+fahrenheit = (celsius) * 1,8 + 32
+#fahrenheit = float(fahrenheit)
+float(fahrenheit)
+print(f"{fahrenheit:.2f}")
+
+
---
+++ 2_decimal_places.py
@@ -0,0 +1,2 @@
+a = 2.222
+print(f"Double of a is {a*2:,.2f}")
